e523be2d2652669c9947e4c6a6ab1d314932c6a7
mr2rm <mrez.rahmani@gmail.com>
2025-09-01 13:20:07 +0200

    chore(nvim): remove old Neovim config

--- nvim/lua/globals/init.lua
+++
@@ -1,2 +0,0 @@
-require 'globals.options'
-require 'globals.keymaps'
--- nvim/lua/plugins/treesitter/nvim-ts-autotag.lua
+++
@@ -1,9 +0,0 @@
-local LazyFile = require('utils').LazyFile
-
-return {
-  {
-    'windwp/nvim-ts-autotag',
-    event = LazyFile,
-    opts = {},
-  },
-}
