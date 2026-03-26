d67683cb7a85bcc50d2cd6250505dbd3981d5315
mr2rm <mrez.rahmani@gmail.com>
2026-03-26 01:18:04 +0100

    fix(nvim): fix markdown-preview by updating build command

--- dot_config/nvim/lua/plugins/utils/markdown-preview.lua
+++ dot_config/nvim/lua/plugins/utils/markdown-preview.lua
@@ -1,20 +1,17 @@
 return {
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-    keys = {
-      {
-        '<leader>p',
-        ft = 'markdown',
-        '<cmd>MarkdownPreviewToggle<cr>',
-        desc = 'Toggle [P]review',
-      },
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = ':call mkdp#util#install()',
+  keys = {
+    {
+      '<leader>p',
+      ft = 'markdown',
+      '<cmd>MarkdownPreviewToggle<cr>',
+      desc = 'Toggle [P]review',
     },
-    config = function()
-      vim.cmd [[do FileType]]
-    end,
   },
+  config = function()
+    vim.cmd [[do FileType]]
+  end,
 }
