ff3ce49d2f812971d1fc3854e53dfac6389318e8
mr2rm <mrez.rahmani@gmail.com>
2026-04-18 23:43:01 +0200

    feat(nvim): install vim-rzip for Yarn SDK in PnP installation

---
+++ dot_config/nvim/lua/plugins/utils/vim-rzip.lua
@@ -0,0 +1,5 @@
+return { -- Recursively browsing and writing Zip files
+  -- NOTE: Required for Yarn SDK in PnP installation:
+  -- https://yarnpkg.com/getting-started/editor-sdks#neovim-native-lsp
+  'lbrayner/vim-rzip',
+}
