26d4cb5c50fff2845d726eac61cd0e587d1c2190
mr2rm <mrez.rahmani@gmail.com>
2026-04-24 21:27:42 +0200

    fix(nvim): fix generating JSDoc / TSDoc with Neogen

--- dot_config/nvim/lua/plugins/coding/neogen.lua
+++ dot_config/nvim/lua/plugins/coding/neogen.lua
@@ -1,21 +1,29 @@
 return {
-  {
-    'danymat/neogen',
-    dependencies = {
-      'L3MON4D3/LuaSnip',
-    },
-    cmd = 'Neogen',
-    opts = {
-      snippet_engine = 'luasnip',
-    },
-    keys = {
-      {
-        '<leader>cn',
-        function()
+  'danymat/neogen',
+  dependencies = {
+    'L3MON4D3/LuaSnip',
+  },
+  cmd = 'Neogen',
+  opts = {
+    snippet_engine = 'luasnip',
+  },
+  keys = {
+    {
+      '<leader>cn',
+      function()
+        local func_ft = {
+          'javascript',
+          'javascriptreact',
+          'typescript',
+          'typescriptreact',
+        }
+        if vim.tbl_contains(func_ft, vim.bo.filetype) then
+          require('neogen').generate { type = 'func' }
+        else
           require('neogen').generate()
-        end,
-        desc = 'A[n]notate',
-      },
+        end
+      end,
+      desc = 'A[n]notate',
     },
   },
 }
